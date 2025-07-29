506957df735a66a82e7bd4f050e2e1927e59df8f
immxrtlabeast <imnomakj@mail.ru>
2025-07-30 01:30:50 +0300

    init + kafka

---
+++ internal/pkg/kafka/consumer.go
@@ -0,0 +1,39 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Consumer struct {
+	reader *kafka.Reader
+}
+
+func NewConsumer(brokers []string, topic, groupID string) *Consumer {
+	return &Consumer{
+		reader: kafka.NewReader(kafka.ReaderConfig{
+			Brokers: brokers,
+			Topic:   topic,
+			GroupID: groupID,
+		}),
+	}
+}
+
+func (c *Consumer) ReadEvent(ctx context.Context, v interface{}) (kafka.Message, error) {
+	msg, err := c.reader.ReadMessage(ctx)
+	if err != nil {
+		return msg, err
+	}
+
+	if err := json.Unmarshal(msg.Value, v); err != nil {
+		return msg, err
+	}
+
+	return msg, nil
+}
+
+func (c *Consumer) Close() error {
+	return c.reader.Close()
+}
