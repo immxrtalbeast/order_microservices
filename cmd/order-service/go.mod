83216ebba63fc08ac7b1a3beeb14bb161a1178cc
Sergeich <serj.reintov228@gmail.com>
2025-08-01 20:19:31 +0300

    start order-service

--- cmd/order-service/go.mod
+++ cmd/order-service/go.mod
@@ -1,3 +1,26 @@
-module immxrtalbeast/order_microservices/order-service
+module immxrtalbeast/order_microservices/cmd/order-service
 
 go 1.24.5
+
+require (
+	github.com/google/uuid v1.6.0
+	gorm.io/gorm v1.30.1
+	google.golang.org/protobuf v1.36.6 
+	google.golang.org/grpc v1.74.2 
+	
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.48 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250528174236-200df99c418a // indirect
+	google.golang.org/grpc v1.74.2 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+	gorm.io/gorm v1.30.1 // indirect
+)
---
+++ cmd/order-service/internal/domain/order.go
@@ -0,0 +1,33 @@
+package domain
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Order struct {
+	ID        uuid.UUID   `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	UserID    uuid.UUID   `gorm:"type:uuid;not null;index"` // Связь с пользователем
+	Items     []OrderItem `gorm:"foreignKey:OrderID;constraint:OnDelete:CASCADE"`
+	Total     float64     `gorm:"type:decimal(10,2);not null"`
+	Status    string      `gorm:"type:varchar(20);not null;default:'CREATED'"`
+	CreatedAt time.Time   `gorm:"autoCreateTime"`
+	UpdatedAt time.Time   `gorm:"autoUpdateTime"`
+}
+
+type OrderItem struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	OrderID   uuid.UUID `gorm:"type:uuid;not null;index"` // Внешний ключ
+	ProductID uuid.UUID `gorm:"type:uuid;not null"`
+	Quantity  int       `gorm:"not null"`
+	Price     float64   `gorm:"type:decimal(10,2);not null"`
+}
+
+type OrderRepository interface {
+	SaveOrder(ctx context.Context, order *Order) (uuid.UUID, error)
+	GetOrder(ctx context.Context, orderID uuid.UUID) (Order, error)
+	UpdateOrderStatus(ctx context.Context, orderID uuid.UUID, status string) error
+	ListOrdersByUser(ctx context.Context, userID uuid.UUID, limit, offset int) ([]Order, error)
+}
