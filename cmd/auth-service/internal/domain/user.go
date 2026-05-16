890b29adc83c821697496f9238d3e0394efbdead
Sergeich <serj.reintov228@gmail.com>
2026-05-16 22:50:57 +0300

    feat: add admin user domain flag

--- cmd/auth-service/internal/domain/user.go
+++ cmd/auth-service/internal/domain/user.go
@@ -2,18 +2,25 @@ package domain
 
 import (
 	"context"
+	"errors"
 
 	"github.com/google/uuid"
+)
+
+var (
+	ErrUserExists   = errors.New("user already exists")
+	ErrUserNotFound = errors.New("user not found")
 )
 
 type User struct {
 	ID       uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	Email    string    `gorm:"unique;not null"`
 	PassHash []byte    `gorm:"not null"`
+	IsAdmin  bool      `gorm:"not null;default:false"`
 }
 
 type UserRepository interface {
 	SaveUser(ctx context.Context, user *User) (uid uuid.UUID, err error)
 	User(ctx context.Context, email string) (User, error)
-	// IsAdmin(ctx context.Context, uid uuid.UUID) (bool, error)
+	IsAdmin(ctx context.Context, uid uuid.UUID) (bool, error)
 }
